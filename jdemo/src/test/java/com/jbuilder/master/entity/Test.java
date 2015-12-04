2f65ce0b2eac90ddf0db074d5cc2d4317d2e728c
lenovo <lenovo@Wangfujun>
2015-12-04 18:33:33 +0800

    乱七八糟

---
+++ jdemo/src/test/java/com/jbuilder/master/entity/Test.java
@@ -0,0 +1,13 @@
+package com.jbuilder.master.entity;
+/**
+ * @author wangfujun
+ * @version 1.0
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
