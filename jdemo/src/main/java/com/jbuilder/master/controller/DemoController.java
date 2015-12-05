a0c8b17b6fa72ffbca0c63d7031ff5122b4427f3
alittleant <623097555@qq.com>
2015-12-05 20:27:28 +0800

    修改log输出样式

--- jdemo/src/main/java/com/jbuilder/master/controller/DemoController.java
+++ jdemo/src/main/java/com/jbuilder/master/controller/DemoController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,15 @@ import com.jbuilder.master.service.DemoService;
 @Controller
 public class DemoController {
 	
+	private static final Log log = LogFactory.getLog(DemoController.class);
+	
 	@Autowired
 	private DemoService demoService;
 
 	@ResponseBody
 	@RequestMapping(value="demo/get.do", method = RequestMethod.GET)
 	public Map<String, Object> get(HttpServletRequest request , HttpServletResponse response) {
+		log.debug("收到请求开始执行......");
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("isSuccess", 1);
 		map.put("message", "土豪新标准天天吃若干青岛大虾!");
@@ -33,6 +38,7 @@ public class DemoController {
 				e.printStackTrace();
 			}
 		}
+		log.debug("请求处理结束......");
 		return map;
 	}
 }
